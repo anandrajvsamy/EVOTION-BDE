2d40e0ff4b5220eb136b8160d4ad63d2eb48efd0
Anandraj <anandraj.venkatasamy@deltacapita.com>
2019-10-12 16:25:29 +0530

    Added the interface to convert csv to Hbase table

---
+++ CSV2Table/src/workflow/tasks/Job.java
@@ -0,0 +1,10 @@
+package workflow.tasks;
+
+public interface Job {
+    void init(Object... varArgs) throws Exception;
+
+    void execute(Object... varArgs) throws Exception;
+
+    void output(Object... varArgs) throws Exception;
+}
+
