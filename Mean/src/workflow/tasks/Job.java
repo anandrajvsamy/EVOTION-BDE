beea5e166515a7938bf2fa550c1f57e105795dff
Anandraj <anandraj.venkatasamy@deltacapita.com>
2020-01-10 13:15:29 +0530

    Added job interface for computing mean

---
+++ Mean/src/workflow/tasks/Job.java
@@ -0,0 +1,9 @@
+package workflow.tasks;
+
+public interface Job {
+    void init(Object... varArgs) throws Exception;
+
+    void execute(Object... varArgs) throws Exception;
+
+    void output(Object... varArgs) throws Exception;
+}
